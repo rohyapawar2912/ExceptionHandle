6dd3935c8114011bad195be5c0b8b90f911f7fc4
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-07-18 09:20:08 +0530

    [Rohit]Add: // add all files

---
+++ ExceptionHandle_Demo/DivideByZero_Exception.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandle_Demo
+{
+    public class DivideByZero_Exception
+    {
+        // it will handle error generated form divided by zero
+        public void Custom()
+        {
+            int x = 0;
+            try
+            {
+                int a = 100 / x;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+    }
+}
---
+++ ExceptionHandle_Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandle_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DivideByZero_Exception divideByZero_Exception = new DivideByZero_Exception();
+            divideByZero_Exception.Custom();
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+    }
+}
